253c2440a368fe131dceebdb23da7666ece41657
Tanisha-2010 <104196498+Tanisha-2010@users.noreply.github.com>
2022-10-04 13:00:38 +0530

    completed!!!

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+difference = 0;
+leftWrist_x = 0;
+rightWrist_x = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(570, 500);
@@ -6,7 +10,7 @@ function setup() {
     canvas.position(890, 170);
 
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('poses', gotPoses);
+    poseNet.on('pose', gotPoses);
 }
 
 function modelLoaded() {
@@ -15,10 +19,20 @@ function modelLoaded() {
 
 function draw() {
     background("lightyellow");
+
+    document.getElementById("size").innerHTML = "The Height and Width of the Text is " + difference + "px";
+    textSize(difference);
+    stroke("pink");
+    text("Focus on the Outcome, not on the Obstacle", 10, 340);
 }
 
 function gotPoses(result) {
     if (result.length > 0) {
         console.log(result);
+
+        leftWrist_x = result[0].pose.leftWrist.x;
+        rightWrist_x = result[0].pose.rightWrist.x;
+        difference = floor(leftWrist_x - rightWrist_x);
+        console.log("Left Wrist =" + leftWrist_x + "Right Wrist = " + rightWrist_x + "Diffrence = " + difference);
     }
 }
